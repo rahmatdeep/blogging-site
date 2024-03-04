87e7bd8d70a3d641b226e7b5462ec5bc185667f2
rahmatdeep <rahmatdeep@gmail.com>
2024-03-04 21:26:16 +0530

    add blog route handlers

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -1,4 +1,13 @@
 import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+
+enum ResponseStatus {
+  Success = 200,
+  NotFound = 404,
+  AlreadyExsists = 403,
+  ServorError = 500,
+}
 
 export const blogRouter = new Hono<{
   Bindings: {
@@ -7,14 +16,94 @@ export const blogRouter = new Hono<{
   };
 }>();
 
-blogRouter.post("/", (c) => {
-  return c.text("blog POST route");
+blogRouter.use("/*", async (c, next) => {
+  await next();
 });
 
-blogRouter.put("/", (c) => {
-  return c.text("blog PUT route");
+blogRouter.post("/", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+
+  try {
+    const post = await prisma.post.create({
+      data: {
+        title: body.title,
+        content: body.content,
+        authorId: "1",
+      },
+      select: {
+        id: true,
+      },
+    });
+    return c.json({ id: post.id });
+  } catch (e) {
+    console.log(e);
+    c.status(ResponseStatus.ServorError);
+    c.json({ msg: "Internal Servor Error" });
+  }
 });
 
-blogRouter.get("/bulk", (c) => {
-  return c.text("blog GET route where id is ");
+blogRouter.put("/", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+
+  try {
+    const post = await prisma.post.update({
+      where: {
+        id: body.id,
+      },
+      data: {
+        title: body.title,
+        content: body.content,
+      },
+    });
+    return c.json({ id: post.id });
+  } catch (e) {
+    console.log(e);
+    c.status(ResponseStatus.ServorError);
+    return c.json({ msg: "Internal Servor Error" });
+  }
+});
+
+blogRouter.get("/", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+
+  try {
+    const post = await prisma.post.findFirst({
+      where: {
+        id: body.id,
+      },
+    });
+    return c.json({ post });
+  } catch (e) {
+    console.log(e);
+    c.status(ResponseStatus.ServorError);
+    c.json({ msg: "Internal Servor Error" });
+  }
+});
+
+blogRouter.get("/bulk", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  try {
+    const posts = await prisma.post.findMany();
+
+    return c.json({ posts });
+  } catch (e) {
+    console.log(e);
+    c.status(ResponseStatus.ServorError);
+    return c.json({ msg: "Internal Servor Error" });
+  }
 });
