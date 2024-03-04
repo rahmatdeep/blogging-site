6091185e6dc9677335fbafc400602d72f3136949
rahmatdeep <rahmatdeep@gmail.com>
2024-03-04 21:57:13 +0530

    add auth middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,7 @@
 import { Hono } from "hono";
-import { PrismaClient } from "@prisma/client/edge";
-import { withAccelerate } from "@prisma/extension-accelerate";
-import { sign } from "hono/jwt";
 import { userRouter } from "./routes/user";
 import { blogRouter } from "./routes/blog";
 
-enum ResponseStatus {
-  Success = 200,
-  NotFound = 404,
-  AlreadyExsists = 403,
-  ServorError = 500,
-}
 
 const app = new Hono<{
   Bindings: {
--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -1,12 +1,14 @@
 import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
+import { verify } from "hono/jwt";
 
 enum ResponseStatus {
   Success = 200,
   NotFound = 404,
   AlreadyExsists = 403,
   ServorError = 500,
+  UnAuthorized = 401,
 }
 
 export const blogRouter = new Hono<{
@@ -14,10 +16,27 @@ export const blogRouter = new Hono<{
     DATABASE_URL: string;
     JWT_SECRET: string;
   };
+  Variables: {
+    userId: string;
+  };
 }>();
 
 blogRouter.use("/*", async (c, next) => {
-  await next();
+  const authHeader = c.req.header("Authorization");
+
+  if (!authHeader) {
+    c.status(ResponseStatus.UnAuthorized);
+    return c.json({ msg: "Access Denied" });
+  }
+  const token = authHeader.split(" ")[1];
+  try {
+    const payload = await verify(token, c.env.JWT_SECRET);
+    c.set("userId", payload.id);
+    await next();
+  } catch (e) {
+    c.status(ResponseStatus.UnAuthorized);
+    return c.json({ msg: "Access Denied" });
+  }
 });
 
 blogRouter.post("/", async (c) => {
@@ -26,13 +45,13 @@ blogRouter.post("/", async (c) => {
   }).$extends(withAccelerate());
 
   const body = await c.req.json();
-
+  const authorId = c.get("userId");
   try {
     const post = await prisma.post.create({
       data: {
         title: body.title,
         content: body.content,
-        authorId: "1",
+        authorId: authorId,
       },
       select: {
         id: true,
