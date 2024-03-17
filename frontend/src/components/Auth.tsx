e15ef977ea91402856b6e177afdd1f976f17793b
rahmatdeep <rahmatdeep@gmail.com>
2024-03-18 00:06:51 +0530

    add react form-hooks for signin and signup page

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -1,22 +1,19 @@
 import { SingupInput } from "@rahmatdeep/blogging-app-common";
 import axios from "axios";
-import { ChangeEvent, useState } from "react";
+
 import { Link, useNavigate } from "react-router-dom";
 import { BACKEND_URl } from "../config";
+import { SubmitHandler, UseFormRegister, useForm } from "react-hook-form";
 
 export default function Auth({ type }: { type: "signup" | "signin" }) {
-  const [postInputs, setPostInputs] = useState<SingupInput>({
-    name: "",
-    email: "",
-    password: "",
-  });
+  const { register, handleSubmit } = useForm<SingupInput>();
 
   const navigate = useNavigate();
-  async function sendRequest() {
+  const sendRequest: SubmitHandler<SingupInput> = async (data) => {
     try {
       const response = await axios.post(
         `${BACKEND_URl}/api/v1/user/${type === "signup" ? "signup" : "signin"}`,
-        postInputs
+        data
       );
       const jwt = response.data.jwt;
       localStorage.setItem("token", `Bearer ${jwt}`);
@@ -26,17 +23,12 @@ export default function Auth({ type }: { type: "signup" | "signin" }) {
         console.log(e.message);
         if (e.response) {
           alert(e.response.data.msg);
-          setPostInputs({
-            name: "",
-            email: "",
-            password: "",
-          });
         }
       } else {
         console.error("An unexpected error occurred", e);
       }
     }
-  }
+  };
   return (
     <>
       <div className="h-screen flex justify-center flex-col">
@@ -65,34 +57,25 @@ export default function Auth({ type }: { type: "signup" | "signin" }) {
                 <LabeledInput
                   label="Name"
                   placeholder="Rahmatdeep Singh"
-                  value={postInputs.name || ""}
-                  onChange={(e) => {
-                    setPostInputs((c) => ({
-                      ...c,
-                      name: e.target.value,
-                    }));
-                  }}
+                  register={register}
+                  name="name"
                 />
               )}
               <LabeledInput
                 label="email"
-                value={postInputs.email}
+                name="email"
                 placeholder="rahmatdeep@gmail.com"
-                onChange={(e) => {
-                  setPostInputs((c) => ({ ...c, email: e.target.value }));
-                }}
+                register={register}
               />
               <LabeledInput
                 label="password"
                 placeholder="password"
-                value={postInputs.password}
-                onChange={(e) => {
-                  setPostInputs((c) => ({ ...c, password: e.target.value }));
-                }}
+                register={register}
+                name="password"
                 inputType="password"
               />
               <button
-                onClick={sendRequest}
+                onClick={handleSubmit(sendRequest)}
                 type="button"
                 className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium w-full rounded-lg text-sm px-5 py-2.5 me-2 mb-2 mt-4"
               >
@@ -107,19 +90,19 @@ export default function Auth({ type }: { type: "signup" | "signin" }) {
 }
 
 interface LabelledInputType {
-  value: string;
   label: string;
   placeholder: string;
-  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  register: UseFormRegister<SingupInput>;
   inputType?: string;
+  name: keyof SingupInput;
 }
 
 function LabeledInput({
-  value,
   label,
   placeholder,
-  onChange,
+  register,
   inputType,
+  name,
 }: LabelledInputType) {
   return (
     <div>
@@ -127,8 +110,7 @@ function LabeledInput({
         {label}
       </label>
       <input
-        value={value}
-        onChange={onChange}
+        {...register(name)}
         type={inputType || "text"}
         className=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
         placeholder={placeholder}
