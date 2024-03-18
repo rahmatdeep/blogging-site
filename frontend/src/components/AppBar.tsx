886183e75fd77c0e359730e6879071500a95877f
rahmatdeep <rahmatdeep@gmail.com>
2024-03-19 00:35:58 +0530

    adjust appbar

--- frontend/src/components/AppBar.tsx
+++ frontend/src/components/AppBar.tsx
@@ -9,13 +9,13 @@ export default function Appbar() {
     <>
       <div className="border-b flex justify-between px-10 py-4 items-center">
         <Link className="cursor-pointer" to={"/posts"}>
-          <div>Blogging App</div>
+          <div className="text-xl font-medium">Blogging App</div>
         </Link>
         <div>
           <Link to={"/publish"}>
             <button
               type="button"
-              className="mr-4 focus:outline-none text-white bg-green-500 hover:bg-green-600 focus:ring-4 focus:ring-green-300 font-medium rounded-2xl text-sm px-3 py-2 "
+              className="mr-4 focus:outline-none text-black bg-slate-100 hover:bg-slate-200  font-small rounded-lg text-md px-3 py-2 "
             >
               New Post
             </button>
