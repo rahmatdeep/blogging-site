a59a9d821ec5ae6a7308f6b15b6aa7bb66f667c9
rahmatdeep <rahmatdeep@gmail.com>
2024-03-18 19:41:56 +0530

    add react-hook-forms to publish component

--- frontend/src/pages/Publish.tsx
+++ frontend/src/pages/Publish.tsx
@@ -3,56 +3,103 @@ import Appbar from "../components/AppBar";
 import { BACKEND_URl } from "../config";
 import { ChangeEvent, ReactNode, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { SubmitHandler, UseFormRegister, useForm } from "react-hook-form";
+
+type PublishTypes = {
+  title: string;
+  content: string;
+};
 
 export default function Publish() {
-  const [title, setTitle] = useState("");
-  const [content, setContent] = useState("");
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { isSubmitting, errors },
+  } = useForm<PublishTypes>();
 
   const navigate = useNavigate();
+
+  const sendRequest: SubmitHandler<PublishTypes> = async (data) => {
+    try {
+      const response = await axios.post(`${BACKEND_URl}/api/v1/post`, data, {
+        headers: {
+          Authorization: localStorage.getItem("token"),
+        },
+      });
+      navigate(`/post/${response.data.id}`);
+    } catch (e: unknown) {
+      if (axios.isAxiosError(e)) {
+        console.log(e.message);
+
+        if (e.response) {
+          setError("root", {
+            message: e.response.data.msg,
+          });
+        }
+      }
+    }
+  };
+
   return (
     <>
       <div>
         <Appbar />
       </div>
       <div className="flex flex-col items-center">
-        <div className="max-w-screen-lg w-full pt-8">
+        <form
+          onSubmit={handleSubmit(sendRequest)}
+          className="max-w-screen-lg w-full pt-8"
+        >
           <input
-            onChange={(e) => {
-              setTitle(e.target.value);
-            }}
+            {...register("title")}
             type="text"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  "
             placeholder="Title"
           ></input>
           <div className="pt-2">
-            <TextEditor
-              onChange={(e) => {
-                setContent(e.target.value);
-              }}
-            >
-              <button
-                onClick={async () => {
-                  const response = await axios.post(
-                    `${BACKEND_URl}/api/v1/post`,
-                    {
-                      title,
-                      content,
-                    },
-                    {
-                      headers: {
-                        Authorization: localStorage.getItem("token"),
-                      },
-                    }
-                  );
-                  navigate(`/post/${response.data.id}`);
-                }}
-                className="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-blue-500 rounded-lg focus:ring-4 focus:ring-blue-200  hover:bg-blue-600"
-              >
-                Publish Post
-              </button>
+            <TextEditor register={register} name="content">
+              {isSubmitting ? (
+                <button
+                  disabled
+                  type="button"
+                  className="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-blue-500 rounded-lg focus:ring-4 focus:ring-blue-200  hover:bg-blue-600"
+                >
+                  <svg
+                    aria-hidden="true"
+                    role="status"
+                    className="inline w-4 h-4 me-3 text-white animate-spin"
+                    viewBox="0 0 100 101"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                      fill="#E5E7EB"
+                    />
+                    <path
+                      d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                      fill="currentColor"
+                    />
+                  </svg>
+                  Posting...
+                </button>
+              ) : (
+                <button
+                  type="submit"
+                  className="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-blue-500 rounded-lg focus:ring-4 focus:ring-blue-200  hover:bg-blue-600"
+                >
+                  Publish Post
+                </button>
+              )}
             </TextEditor>
           </div>
-        </div>
+          {errors.root && (
+            <div className="text-red-500 w-full text-left pt-1">
+              {errors.root.message}
+            </div>
+          )}
+        </form>
       </div>
     </>
   );
@@ -60,16 +107,19 @@ export default function Publish() {
 
 interface TextEditorProps {
   children: ReactNode;
-  onChange: (e: ChangeEvent<HTMLTextAreaElement>) => void;
+
+  register: UseFormRegister<PublishTypes>;
+  name: keyof PublishTypes;
 }
 
 function TextEditor(props: TextEditorProps) {
+  const reg = props.register;
   return (
     <div>
       <div className="w-full  mb-4 border border-gray-300 rounded-lg bg-gray-50 ">
         <div className="px-1 py-1 bg-white rounded-t-lg ">
           <textarea
-            onChange={props.onChange}
+            {...reg(props.name)}
             rows={4}
             className="w-full px-0 text-sm text-gray-900 bg-white border-0  focus:outline-none "
             placeholder="Write the content"
