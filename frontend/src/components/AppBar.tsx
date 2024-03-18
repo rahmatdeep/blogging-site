13943e44f7c5a78f3dde20fdbcbc407f0ec8c236
rahmatdeep <rahmatdeep@gmail.com>
2024-03-18 12:19:10 +0530

    add names to Authbar

--- frontend/src/components/AppBar.tsx
+++ frontend/src/components/AppBar.tsx
@@ -1,7 +1,23 @@
 import { Link } from "react-router-dom";
 import { Avatar } from "./PostCard";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { BACKEND_URl } from "../config";
 
 export default function Appbar() {
+  const [username, setUsername] = useState("");
+
+  useEffect(() => {
+    axios
+      .get(`${BACKEND_URl}/api/v1/user`, {
+        headers: {
+          Authorization: localStorage.getItem("token"),
+        },
+      })
+      .then((respose) => {
+        setUsername(respose.data.name);
+      });
+  }, []);
   return (
     <>
       <div className="border-b flex justify-between px-10 py-4 items-center">
@@ -12,13 +28,13 @@ export default function Appbar() {
           <Link to={"/publish"}>
             <button
               type="button"
-              className="mr-4 focus:outline-none text-white bg-green-500 hover:bg-green-600 focus:ring-4 focus:ring-green-300 font-medium rounded-2xl text-sm px-5 py-2.5 me-2 mb-2"
+              className="mr-4 focus:outline-none text-white bg-green-500 hover:bg-green-600 focus:ring-4 focus:ring-green-300 font-medium rounded-2xl text-sm px-3 py-2 "
             >
               New Post
             </button>
           </Link>
 
-          <Avatar name="Rahmatdeep" size={"big"} />
+          <Avatar name={username} size={"big"} />
         </div>
       </div>
     </>
