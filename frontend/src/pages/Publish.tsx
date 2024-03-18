41d590831e351a2ea3fd63368a17ec02fc438bef
rahmatdeep <rahmatdeep@gmail.com>
2024-03-18 19:42:43 +0530

    fixed imports in Publish component

--- frontend/src/pages/Publish.tsx
+++ frontend/src/pages/Publish.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import Appbar from "../components/AppBar";
 import { BACKEND_URl } from "../config";
-import { ChangeEvent, ReactNode, useState } from "react";
+import { ReactNode } from "react";
 import { useNavigate } from "react-router-dom";
 import { SubmitHandler, UseFormRegister, useForm } from "react-hook-form";
 
