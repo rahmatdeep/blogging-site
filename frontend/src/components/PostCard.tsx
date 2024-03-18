5d7d90cc8b01e11b24d9b6c06105f1db0d678089
rahmatdeep <rahmatdeep@gmail.com>
2024-03-18 17:57:43 +0530

    remove background color of PostCard component

--- frontend/src/components/PostCard.tsx
+++ frontend/src/components/PostCard.tsx
@@ -31,7 +31,7 @@ export default function PostCard({
   return (
     <>
       <Link to={`/post/${id}`}>
-        <div className="p-4 border-b border-slate-200 pb-4 bg-slate-50 w-screen max-w-3xl cursor-pointer">
+        <div className="p-4 border-b border-slate-200 pb-4  w-screen max-w-3xl cursor-pointer">
           <div className="flex">
             <div className="">
               <Avatar name={authorName} size={"small"} />
