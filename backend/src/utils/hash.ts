c8d7a79308d7ef0143e4a1352fadf6650fe60266
rahmatdeep <rahmatdeep@gmail.com>
2024-03-20 21:26:19 +0530

    remove key pair

--- backend/src/utils/hash.ts
+++ backend/src/utils/hash.ts
@@ -1,18 +1,5 @@
 export async function hashPassword(password: string) {
   try {
-    const keyPair = await crypto.subtle.generateKey(
-      {
-        name: "RSA-OAEP",
-        modulusLength: 2048,
-        publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
-        hash: "SHA-256",
-      },
-      true,
-      ["encrypt", "decrypt"]
-    );
-
-    const publicKey = await crypto.subtle.exportKey("spki", keyPair.publicKey);
-
     const passwordBuffer = new TextEncoder().encode(password);
 
     const hashBuffer = await crypto.subtle.digest("SHA-256", passwordBuffer);
