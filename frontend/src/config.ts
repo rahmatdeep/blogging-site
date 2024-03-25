3db6508b50ee54df846dc9010409914dc8c7e49d
rahmatdeep <rahmatdeep@gmail.com>
2024-03-25 16:43:08 +0530

    fix config file

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,2 +1,1 @@
-// export const BACKEND_URl = "https://blogging-app-backend.rahmatdeep.workers.dev";
-export const BACKEND_URl = "http://localhost:8787"
+export const BACKEND_URl = "https://blogging-app-backend.rahmatdeep.workers.dev";
